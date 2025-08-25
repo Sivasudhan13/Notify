b877daf3fd02205ee244b5cc85d4097c9f7d82d6
Sivasudhan13 <sivasudhan87@gmail.com>
2025-08-25 06:21:00 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const Base_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: Base_URL,
 });
 
 export default api;
